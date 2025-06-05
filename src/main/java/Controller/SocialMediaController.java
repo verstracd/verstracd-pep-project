5d435dae938e08a0abe07e521bbdc09f8c2be4c9
caleb verstraete <caleb.rev2@gmail.com>
2025-06-05 21:11:18 +0000

    small change to commit and see if tests run

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -12,9 +12,10 @@ import java.util.Map;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
- * found in readme.md as well as the test cases. You should
+ * found in readme.md as well as the test cases. You shoul
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
 public class SocialMediaController {
     MessageService messageService;
     AccountService accountService;
